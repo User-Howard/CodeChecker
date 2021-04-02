93471d6288b93ca35dd53e9390754b3ebdd68d87
User-Howard <howard20060216@gmail.com>
2021-04-02 18:26:52 +0800

    _

---
+++ 測試.sh
@@ -0,0 +1,23 @@
+read -p "Please enter your folder:" t_PATH
+read -p "Please enter your file(.exe):" RPOGRAM_FILE
+echo $RPOGRAM_FILE
+
+all_correct=true
+for f in $t_PATH/*/*/*.in
+do
+    echo "${f}"
+    $RPOGRAM_FILE < $f > tmp
+    ./diff.exe -c -b tmp ${f%.*}.ans
+    if [ $? != 0 ]; then
+        all_correct=false
+        break
+    fi
+done
+if [ $all_correct == true ]; then
+    echo ""
+    echo ""
+    echo "ALL PASS"
+    echo ""
+    echo ""
+fi
+read -p "Press ENTER to exit." enter
